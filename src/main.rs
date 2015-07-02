ae2a56b55fa1874d06fca9f3910fd48772a51510
Kristoffer Semelka <soderstroff@gmail.com>
2015-07-01 23:33:33 -0700

    Moved functions on cons to an impl, and implemented Display trait for Data.

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,13 @@
 #![allow(dead_code)]
 #![allow(unused_variables)]
 #![allow(unused_mut)]
+#![allow(unused_must_use)]
+
 
 use std::slice::Iter;
 use std::iter::Peekable;
 use std::collections::HashMap;
+use std::fmt;
 use Data::{Nil, Symbol, Float, Cons};
 
 #[derive(PartialEq,Debug)]
@@ -15,85 +18,77 @@ enum Data {
     Cons(Box<(Data,Data)>)
 }
 
-fn cons (car:Data, cdr:Data) -> Data {
-    Data::Cons( Box::new( (car,cdr) ) )
-}
-
-fn atom(v:&Data) -> bool {
-    match *v {
+impl Data {
+    fn atom(&self) -> bool {
+        match *self {
         Cons(_) => false,
-        _ => true,
+            _ => true,
+        }
+    }
+
+    fn car (&self) -> &Data {
+        match *self {
+            Cons(ref c) => &c.0,
+            ref v @ _ => v,
+        }
+    }
+
+    fn cdr (&self) -> &Data {
+        match *self {
+            Cons(ref c) => &c.1,
+            ref v @ _ => v,
+        }
+    }
+
+    fn cadr (&self) -> &Data {
+        self.cdr().car()
+    }
+
+    fn nreverse (self) -> Data{
+        let mut curr = self;
+        let mut prv = Nil;
+        
+        while curr != Nil {
+            match curr {
+                Cons(ref mut c) =>{
+                    let next = &mut c.1;
+                    std::mem::swap(next, &mut prv);
+                }
+                _ => panic!("Not a proper list!"),
+            };
+            std::mem::swap(&mut curr, &mut prv);
+        }
+        prv
     }
 }
 
-fn nreverse (list: Data) -> Data{
-    let mut curr =  list;
-    let mut prv = Nil;
-    
-    while curr != Nil {
-        match curr {
-            Cons(ref mut c) =>{
-                let next = &mut c.1;
-                std::mem::swap(next, &mut prv);
-            }
-            _ => panic!("Not a proper list!"),
-        };
-        std::mem::swap(&mut curr, &mut prv);
-    }
-    prv
-}
-
-fn car (d:&Data) -> &Data {
-    match *d {
-        Cons(ref c) => &c.0,
-        ref v @ _ => v,
-    }
-}
-
-fn cdr (d:&Data) -> &Data {
-    match *d {
-        Cons(ref c) => &c.1,
-        ref v @ _ => v,
-    }
-}
-
-fn cadr (d:&Data) -> &Data {
-    car(cdr(d))
-}
-
-fn print(list:&Data) {
-    /* Pretty prints a list. Calls a helper function. */
-    printh(list);
-    print!("\n");
-}
-
-fn printh(list:&Data) {
-    match *list {
-        Float(s) => print!("{}", s),
-        Symbol(ref s) => print!("{}", *s),
-        Nil => print!("Nil"),
-        Cons(ref c) => {
-            print!("(");
-            printh(&c.0);
-            let mut focus = &c.1;
-            while *focus != Nil {
-                match *focus {
-                    Nil => {unreachable!();},
-                    Cons(ref c) => {
-                        print!(" ");
-                        printh(&c.0);
-                        focus = &c.1;
-                    }
-                    _ => { print!(" . ");
-                           printh(&focus);
-                           print!(")");
-                           return;
+impl fmt::Display for Data {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match *self {
+            Float(s) => write!(f, "{}", s),
+            Symbol(ref s) => write!(f, "{}", *s),
+            Nil => write!(f, "Nil"),
+            Cons(ref c) => {
+                write!(f, "({}", &c.0);
+                let mut focus = &c.1;
+                while *focus != Nil {
+                    match *focus {
+                        Nil => {unreachable!();},
+                        Cons(ref c) => {
+                            write!(f, " {}", &c.0);
+                            focus = &c.1;
+                        }
+                        _ => return write!(f, " . {})", focus)
                     }
                 }
+                write!(f, ")")
             }
-            print!(")");
         }
-    };
+    }
+}
+    
+fn cons (car:Data, cdr:Data) -> Data {
+    Data::Cons( Box::new( (car,cdr) ) )
 }
 
 fn tokenize(chars:&str) -> Vec<String>{
@@ -122,7 +117,7 @@ fn read_from_tokens(tokens:&mut Peekable<Iter<String>>) -> Data {
             l = cons(read_from_tokens(tokens), l);
         }
         tokens.next();
-        return nreverse(l);
+        return l.nreverse();
     }
             
     return Data::Symbol(token.to_string())
@@ -134,10 +129,10 @@ fn parse(program:&str) -> Data {
 
 fn main() {
     let parsed = parse("(begin (define r 10) (* pi (* r r)))");
-    print(&parsed);
-    print(&cons(Symbol("Love".to_string()), Symbol("You".to_string())));
-    print(&cons(Nil, Nil));
-    print(&cons(Float(1.0), Float(2.0)));
-    print(&cons(Nil, Symbol("Test".to_string())));
-    print(&parse("(1 2 3 ())"));
+    println!("{}",&parsed);
+    println!("{}",&cons(Symbol("Love".to_string()), Symbol("You".to_string())));
+    println!("{}", cons(Nil, Nil));
+    println!("{}",(&cons(Float(1.0), Float(2.0))));
+    println!("{}",&cons(Nil, Symbol("Test".to_string())));
+    println!("{}",&parse("(1 2 3 ())"));
 }
